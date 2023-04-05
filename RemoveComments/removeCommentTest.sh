cc0a0997f5ffe098a8a51212aa539541faf528e9
Luca Mock <luca.mock@ringieraxelspringer.ch>
2023-04-05 09:58:59 +0200

    more test files

---
+++ RemoveComments/removeCommentTest.sh
@@ -0,0 +1,34 @@
+# comments
+# comments
+# comments
+# comments
+
+# comments
+echo "no comment"
+
+# comments
+# comments
+# comments
+# comments
+
+# comments
+# comments
+# comments
+# comments
+
+# comments
+# comments
+# comments
+# comments
+# comments
+
+# comments
+
+echo "no comment"
+
+echo "no comment"
+
+
+echo "no comment"
+
+# comments
--- RemoveComments/removeCommentsTest.js
+++ RemoveComments/removeCommentsTest.js
@@ -1,4 +1,14 @@
+// test
+
 const a = 1;
 const b = 2;
+
+// test 2
+
 const c = a + b;
+
+// test 3
+
 console.log("xxx", c);
+
+// test 4
