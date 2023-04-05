a1bc6842865076698b8850831858b795929a1b63
Luca Mock <luca.mock@ringieraxelspringer.ch>
2023-04-05 09:01:42 +0200

    remove comments update

--- RemoveComments/removeComments.sh
+++ RemoveComments/removeComments.sh
@@ -1,11 +1,18 @@
+#!/bin/bash
+
+# Prüfe, ob eine Datei angegeben wurde
 if [ -z "$1" ]; then
   echo "Bitte geben Sie eine Datei an."
   exit 1
 fi
+
+# Prüfe, ob die Datei existiert
 if [ ! -f "$1" ]; then
   echo "Die Datei $1 existiert nicht."
   exit 1
 fi
+
+# Entferne Kommentare und leere Zeilen aus der Datei
 if [[ "$1" == *.sh ]]; then
   sed -i '' -e '/^\s*#/d' -e '/^[[:space:]]*$/d' "$1"
 elif [[ "$1" == *.js ]]; then
@@ -14,4 +21,5 @@ else
   echo "Kommentare können nur aus .sh- oder .js-Dateien entfernt werden."
   exit 1
 fi
+
 echo "Kommentare und leere Zeilen wurden aus der Datei $1 entfernt."
